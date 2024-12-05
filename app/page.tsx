ff61da018f5b861eff412c4b3014e167a07a6b7b
zasuh <zane.suhadolnik@dlabs.si>
2024-12-05 14:15:25 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Žane Suhadolnik
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a web developer with 5+ YOE from Ljubljana, Slovenia with a focus on the frontend. I love simplicity in both design and code while maintaining a high level of quality.`}
       </p>
-      <div className="my-8">
+      {/* <div className="my-8">
         <BlogPosts />
-      </div>
+      </div> */}
     </section>
-  )
+  );
 }
