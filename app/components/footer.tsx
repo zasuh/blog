1959fffb0af0f7016646ed3013b6a35b7283aa96
zasuh <zane.suhadolnik@dlabs.si>
2025-03-10 14:48:05 +0100

    fix flourish image

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,31 +1,13 @@
 import Image from "next/image";
-import Flourish from "../public/Flourish-1-flipped.png";
 import { cn } from "app/utils/cn";
 import localFont from "next/font/local";
 const CloisterBlack = localFont({ src: "../public/CloisterBlack.ttf" });
-
-function ArrowIcon() {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-}
 
 export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center w-full mt-8 p-4">
       <Image
-        src={Flourish}
+        src="../public/flourish.png"
         width={200}
         height={200}
         alt="Flourished"
